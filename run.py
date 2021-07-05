0544c64aa54e88c0602de55edea8fc71e2127dfd
dhruv2102 <kumardh@tcd.ie>
2021-07-05 14:41:43 +0000

    Added check for valid values

--- run.py
+++ run.py
@@ -23,11 +23,30 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    
+    sales_data = data_str.split(',')
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Total values is 6
+    """
+    try:
+        [int(value) for value in values]
+        if (len(values)) != 6:
+            raise ValueError(
+                f"Eactly 6 values required, you provided {len(values)} values"
+        )
+
+    except ValueError as e:
+        print(f"Invalid data: {e}, try again")
+        get_sales_data()
+
 
 
 def main():
     get_sales_data()
+
 
 if __name__ == "__main__":
     main()
